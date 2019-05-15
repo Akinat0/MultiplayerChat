92d00830d7f16ec0f1c6c4e13bf608d3d4e64f0a
Амир Абу махади <abu.amir@mail.ru>
2019-05-15 16:14:00 +0300

    Init commit

---
+++ Assets/Scripts/GameHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameHelper : MonoBehaviour
+{
+
+    public InputField inputField;
+    public Text textBlock;
+
+    private PlayerHelper _currentPlayer;
+
+    public PlayerHelper CurrentPlayer
+    {
+        get {return _currentPlayer;}
+
+        set { _currentPlayer = value;}
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Send(){
+        CurrentPlayer.Send(inputField.text);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerHelper : NetworkBehaviour {
+
+    GameHelper _gameHelper;
+
+	// Use this for initialization
+	void Start () {
+        _gameHelper = GameObject.FindObjectOfType<GameHelper>();
+
+        if(isLocalPlayer){
+            _gameHelper.CurrentPlayer = this;
+        }
+
+	}
+
+    public void Send(string msg){
+        string id = GetComponent<NetworkIdentity>().netId.ToString();
+        CmdSend(id, msg);
+    }
+
+    [Command]
+    public void CmdSend(string id, string msg){
+        Debug.Log("Send = " + msg);
+        RpcSend(id, msg);
+    }
+
+    [ClientRpc]
+    public void RpcSend(string id, string msg){
+        _gameHelper.textBlock.text += System.Environment.NewLine + id + ": "+ msg;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
